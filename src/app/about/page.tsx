09f38d34cf652322f74d06309b14e7fb5c8d4f4a
shuai.man <shuai.man@nington.com>
2025-06-10 16:46:59 +0800

    update

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+    return (
+        <>
+            <span className={'text-4xl text-red-600'}>about page</span>
+        </>
+    );
+}
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,4 +1,4 @@
-export default function SignIn() {
+export default function Signin() {
     return (
         <>
             <span className={'text-4xl text-red-600'}>test page</span>
