f25abdeeee35524906bafdc36b1447e764273e04
shuai.man <shuai.man@nington.com>
2025-06-12 11:52:28 +0800

    update

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+import SignUp from '@/component/application/centered-sign-up/App'
+
+export default function App() {
+    return (
+        <>
+            <SignUp/>
+        </>
+    );
+}
--- src/component/marketing/minimal-cookie-consent-with-brand-colors/icon.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import type {IconProps} from "@iconify/react";
-
-import {Icon as BaseIcon} from "@iconify/react";
-
-export const Icon = (props: IconProps) => {
-  return <BaseIcon {...props} />;
-};
